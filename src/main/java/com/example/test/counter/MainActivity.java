301027180727398652643d18e0e41f00eff89c1b
ဟိန်းသူရဝင်း <heinthurawynn.v2m@gmail.com>
2017-02-03 15:27:44 +0630

    version

--- src/main/java/com/example/test/counter/MainActivity.java
+++ src/main/java/com/example/test/counter/MainActivity.java
@@ -14,29 +14,30 @@ public class MainActivity extends AppCompatActivity {
     TextView textView2;
     Button button2;
     int count;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button= (Button) findViewById(R.id.button);
-        textView= (TextView) findViewById(R.id.textView);
-        textView2= (TextView) findViewById(R.id.textView2);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        textView2 = (TextView) findViewById(R.id.textView2);
         textView2.setText("This Is The First Page ^_^ ");
-        button2= (Button) findViewById(R.id.button2);
+        button2 = (Button) findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 count++;
-                textView.setText("Number You Click="+count);
+                textView.setText("Number You Click=" + count);
             }
         });
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent it=new Intent(getApplicationContext(),Second.class);
+                Intent it = new Intent(getApplicationContext(), Second.class);
                 startActivity(it);
-                Toast.makeText(getApplicationContext(),"Go to Second",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Go to Second", Toast.LENGTH_SHORT).show();
             }
         });
     }
